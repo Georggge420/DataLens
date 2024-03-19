bb0ff6f687dd9b37ff5c743616c91bf98add2679
Georggge420 <cervantesjorget@gmail.com>
2024-03-19 07:59:58 -0600

    agregando header y rutas de vistas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import HeaderMobile from "@/components/HeaderMobile";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/styles/types.tsx
+++ src/styles/types.tsx
@@ -1,48 +1,12 @@
-import { Icon } from "@iconify/react";
-import { SideVarItem } from './constants';
+export type SideNavItem = {
+    title: string;
+    path?: string;
+    icon?: JSX.Element;
+    submenu?: boolean;
+    subMenuItems?: SideNavItem[];
+}
 
-export const SIDEVARITEMS: SideVarItem[] = [
-    {
-        title: "DataHome",
-        path: "/",
-        icon: <Icon icon="mi" />,
-    },
-    {
-        title: "X",
-        path: "/Xboard",
-        icon: <Icon icon="fa6-brands:x-twitter" />
-    },
-    {
-        title: "Facebook",
-        path: "/facebookboard",
-        icon: <Icon icon="brandico:facebook" />
-    },
-    {
-        title: "Instagram",
-        path: "/instragramboard",
-        icon: <Icon icon="entypo-social:instagram" />
-    },
-    {
-        title: "Spotify",
-        path: "/spotifyboard",
-        icon: <Icon icon="entypo-social:spotify" />,
-    },
-    {
-        title: "Configuracion",
-        icon: <Icon icon="ant-design:setting-outlined" />,
-        submenu: true,
-        subMenuItems: [
-            {
-                title: "Idioma",
-                path: "/Configuracion/Idioma",
-                icon: <Icon icon="flowbite:arrow-sort-letters-outline" />
-            },
-            {
-                title: "Tema",
-                path: "/Configuracion/Tema",
-                icon: <Icon icon="flowbite:moon-solid" />
-            },
-        ]
-    },
-
-]
+export type MenuItemWithSubMenuProps = {
+    item: SideNavItem;
+    toggleOpen: () => void;
+};
